2d94404b58ed2829d0d43319b44ea014b26675ae
Shashank Ginjpalli <shashank.ginjpalli@gmail.com>
2022-01-13 11:09:53 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.968712b44af64033c2fc2d3e21b43cdf.js"
+  "/precache-manifest.203f52b8fb4b8eb50a5a2ba259fc5c33.js"
 );
 
 self.addEventListener('message', (event) => {
